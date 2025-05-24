54cad4865ec0cfd0d530109ea396d59d69e98d07
Faraz Loloei <farazloloei@uni-koblenz.de>
2025-05-24 22:05:46 +0200

    Website commit

---
+++ app/page.tsx
@@ -0,0 +1,32 @@
+import Clients from "@/components/Clients";
+import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import Grid from "@/components/Grid";
+import RecentProjects from "@/components/RecentProjects";
+import { navItems } from "@/data";
+import Experience from "@/components/Experience";
+import Approach from "@/components/ui/Approach";
+import Footer from "@/components/ui/Footer";
+
+export default function Home() {
+  return (
+    <main
+      className="relative bg-black-100 flex 
+     justify-center items-center flex-col overflow-clip
+     mx-auto sm:px-10 px-5"
+    >
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
+        <RecentProjects />
+        <Clients />
+        <Experience />
+        <Approach />
+        <Footer />
+      </div>
+    </main>
+  );
+}
+
+// https://www.youtube.com/watch?v=FTH6Dn3AyIQ&t=7092s&ab_channel=JavaScriptMastery
